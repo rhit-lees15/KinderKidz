2babed1309c9c114a25d1c2b45f591e52e7a2968
KinderKidz <abufarna@rose-hulman.edu>
2024-03-25 16:50:06 -0400

    trying

--- together.py
+++ together.py
@@ -1,3 +1,6 @@
+import pygame
+import os 
+
 import time
 from rpi_ws281x import *
 import argparse
@@ -8,7 +11,7 @@ LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 15     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 65     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -17,13 +20,51 @@ LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 # Define functions which animate LEDs in various ways.
 def colorWipe(strip, color, wait_ms=50):
     """Wipe color across display a pixel at a time."""
-    strip.setPixelColor(50, Color(255, 0, 0))
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
 
-    # for i in range(strip.numPixels()):
-    #     strip.setPixelColor(i, color)
-    #     strip.show()
-    #     time.sleep(wait_ms/1000.0)
 
+def playSound(filepath: str):
+    # pygame.mixer.init()
+    # my_sound = pygame.mixer.Sound(filepath)
+    # my_sound.play()
+    # pygame.time.wait(int(my_sound.get_length() * 1000))
+
+    pygame.mixer.init()
+    
+    pygame.mixer.music.load(filepath)
+
+    pygame.mixer.music.set_volume(0.7)
+
+    pygame.mixer.music.play()
+
+    
+    while True: 
+        print("Press 'p' to pause, 'r' to resume") 
+        print("Press 'e' to exit the program") 
+        query = input("  ") 
+        
+        if query == 'p': 
+    
+            # Pausing the music 
+            pygame.mixer.music.pause()      
+        elif query == 'r': 
+    
+            # Resuming the music 
+            pygame.mixer.music.unpause() 
+        elif query == 'e': 
+    
+            # Stop the mixer 
+            pygame.mixer.music.stop() 
+            break
+
+
+def playOSSound(filepath:str):
+    os.system(f"cvlc {filepath}")
+
+    
 # Main program logic follows:
 if __name__ == '__main__':
     # Process arguments
@@ -35,6 +76,7 @@ if __name__ == '__main__':
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     # Intialize the library (must be called once before other functions).
     strip.begin()
+    playOSSound('sample.wav')
 
     print ('Press Ctrl-C to quit.')
     if not args.clear:
@@ -43,7 +85,6 @@ if __name__ == '__main__':
     try:
 
         while True:
-            # strip.setPixelColor(50, Color(255, 0, 0))
             print ('Color wipe animations.')
             colorWipe(strip, Color(255, 0, 0))  # Red wipe
             colorWipe(strip, Color(0, 255, 0))  # Blue wipe
