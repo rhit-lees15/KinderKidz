53fa2e9946cc1c7730158b043d6ab2c510baad8c
Noor Abufares <abufarna@rose-hulman.edu>
2024-03-26 15:14:44 -0400

    GUI WORKS!!!!

--- GUI Samples/GUIyyyyyyyyyyyyy.py
+++ GUI Samples/GUIyyyyyyyyyyyyy.py
@@ -1,15 +1,19 @@
 import tkinter as tk
 import tkinter.font
 
+## FUTURE NOTE: This will probs change to the next section where the game starts & audio plays
 def button_click():
     print("Button clicked!")
 
 # Create the main window
 root = tk.Tk()
 
+## FONT
+myFont = tkinter.font.Font(family = 'Helvetica', size = 12, weight = "bold")
+
 # Set window size and position it in the center
-window_width = 300
-window_height = 200
+window_width = 600
+window_height = 400
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 x = (screen_width - window_width) // 2
@@ -17,18 +21,15 @@ y = (screen_height - window_height) // 2
 root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 # Create a button
-button = tk.Button(root, text = "Click Me!", command = button_click)
-button.place(relx = 1, rely = 1, anchor = "center")  # Placing button in the center
+button = tk.Button(root, text = "START", font = myFont, command = button_click, bg = 'sea green', height = 5, width = 26)
+button.place(relx = 0.5, rely = 0.5, anchor = "center")  # Placing button in the center
 
 def close():
     root.destroy()
 
-## FONT
-myFont = tkinter.font.Font(family = 'Helvetica', size = 12, weight = "bold")
-
 ## exit button
-exitButton = tk.Button(root, text = "Exit", font = myFont, command = close, bg = 'red', height = 1, width = 26)
-#exitButton.grid(row = 1, column = 1)
+exitButton = tk.Button(root, text = "Exit", font = myFont, command = close, bg = 'brown4', height = 1, width = 52)
+exitButton.pack(side = "bottom")
 
 # exit cleanly
 root.protocol("WM_DELETE_WINDOW", close)
