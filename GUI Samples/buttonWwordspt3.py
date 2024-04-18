1e3479573b9690e59fd140cd3dd010f8247abdf7
KinderKidz <abufarna@rose-hulman.edu>
2024-04-18 16:55:55 -0400

    uncomment

--- GUI Samples/buttonWwordspt3.py
+++ GUI Samples/buttonWwordspt3.py
@@ -6,28 +6,28 @@ import time
 # GPIO Pins for buttons
 BUTTON_PINS = [24, 25, 8, 7, 5, 6, 13, 12]
 
-# Function to generate a random word
-def generateRandomWord(words):
-    return random.choice(words)
+# # Function to generate a random word
+# def generateRandomWord(words):
+#     return random.choice(words)
 
-# Function to remove letters from the alphabet
-def removeLetters(word):
-    alphabet = list(string.ascii_uppercase)
+# # Function to remove letters from the alphabet
+# def removeLetters(word):
+#     alphabet = list(string.ascii_uppercase)
     
-    for letter in word:
-        if letter in alphabet:
-            alphabet.remove(letter)
-    return alphabet
+#     for letter in word:
+#         if letter in alphabet:
+#             alphabet.remove(letter)
+#     return alphabet
 
-# Function to generate additional random letters
-def generateRandomLetters(remainingLetters):
-    return random.sample(remainingLetters, 8 - len(randomWord))
+# # Function to generate additional random letters
+# def generateRandomLetters(remainingLetters):
+#     return random.sample(remainingLetters, 8 - len(randomWord))
 
-# Function to add all letters to one string
-def randomizeLetters(word, letters):
-    allLetters = list(word + ''.join(letters))
-    random.shuffle(allLetters)
-    return ''.join(allLetters)
+# # Function to add all letters to one string
+# def randomizeLetters(word, letters):
+#     allLetters = list(word + ''.join(letters))
+#     random.shuffle(allLetters)
+#     return ''.join(allLetters)
 
 # Function to handle button press event
 def buttonPress(pin):
