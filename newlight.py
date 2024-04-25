83f63895d353592bcc3d9ea402086095b5aaca60
Solbin Lee <lees15@rose-hulman.edu>
2024-04-25 13:03:45 -0400

    .

--- newlight.py
+++ newlight.py
@@ -2,6 +2,7 @@ import time
 from pygame import Color
 import randomLetters
 from rpi_ws281x import *
+import RPI.GPIO as GPIO
 
 # Set Up
 LED_COUNT      = 100      # Number of LED pixels.
@@ -45,6 +46,10 @@ letter_arrays = {'A': A, 'B': B, 'C': C, 'D': D, 'E': E, 'F': F, 'G': G, 'H': H,
 
 wordList = ['CAT', 'DOG', 'CAR', 'BAG', 'HAT', 'LEG', 'ONE', 'MAT']
 
+BUTTON_PIN = 10
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.input(BUTTON_PIN)
+
 
 def turn_off():
     for i in range(strip.numPixels()):
@@ -58,17 +63,21 @@ def display_letter(letter, color):
         strip.show()  
 
 def main():
-        #wordList = ['BATH', 'CARE', 'LOVE']
+        # alphabet = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        # wordList = ['BATH', 'CARE', 'LOVE']
         word = randomLetters.generateRandomWord(wordList)
         print(word)
         # word = 'CAT'
         letters = list(word)
         for letter in letters:
             current_letter = letter_arrays[letter]
-        # alphabet = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-            print(letter)
             display_letter(current_letter, Color(10, 10,10))
-
+            while GPIO.input(BUTTON_PIN) == GPIO.LOW:
+                 display_letter(current_letter, Color(10, 10,10))
+            else:
+                 turn_off()
+                 
+                
 
 if __name__ == '__main__':
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
