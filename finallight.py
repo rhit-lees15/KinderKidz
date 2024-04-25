9e8b3563ba4e700ed22237207390e09b8c7211a6
KinderKidz <abufarna@rose-hulman.edu>
2024-04-25 14:05:10 -0400

    update2

--- finallight.py
+++ finallight.py
@@ -97,24 +97,27 @@ if __name__ == '__main__':
         word = randomLetters.generateRandomWord(wordList)
         print(word)
         letters = list(word)
-        current_index = 0
+        current_index = 1
 
         while True:
             for letter in letters:
+                current_index += 1
+
                 if current_index == 1:
                     current_letter = letter_arrays[letter]
                     display_letter(current_letter, Color(150, 150,150))
                 elif current_index == 2:
+                    print("here222")
                     current_letter = letter_arrays[letter]
                     current_letter = [[num + 100 for num in row] for row in current_letter]
                     display_letter(current_letter, Color(150, 150,150))
 
-                elif current_index ==3:
+                elif current_index == 3:
                     current_letter = letter_arrays[letter]
                     current_letter = [[num + 200 for num in row] for row in current_letter]
 
                     display_letter(current_letter, Color(150, 150,150))
-
+                
 
                 while True:
                     if GPIO.input(BUTTON_PIN) == GPIO.LOW:
