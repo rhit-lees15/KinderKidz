924fba33e8524c4e829affee2b644d77f498958c
KinderKidz <abufarna@rose-hulman.edu>
2024-05-10 16:18:25 -0400

    kjegrd

--- AudioStuff/sound_w_game.py
+++ AudioStuff/sound_w_game.py
@@ -1,9 +1,12 @@
 
+import argparse
 import RPi.GPIO as GPIO 
 import vlc
 import time
 import string
 import random
+from pygame import Color
+from rpi_ws281x import *
 
 # from game_sound import *
 
@@ -30,6 +33,18 @@ def init_vlc(sound_file:str):
     while p.is_playing():
         time.sleep(0.25)
     p.release()
+
+# Initialize lights
+# LED strip configuration:
+LED_COUNT      = 300      # Number of LED pixels.
+LED_PIN        = 18  # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 65     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
     
 
     
