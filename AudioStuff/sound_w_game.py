d6158fe21e46bbb8325ac42e9bb6ae50d78e52dd
KinderKidz <abufarna@rose-hulman.edu>
2024-05-13 23:43:15 -0400

    idk

--- AudioStuff/sound_w_game.py
+++ AudioStuff/sound_w_game.py
@@ -12,8 +12,8 @@ import finallight as light
 
 # Initialize lights
 # LED strip configuration:
-LED_COUNT      = 300      # Number of LED pixels.
-LED_PIN        = 18  # GPIO pin connected to the pixels (18 uses PWM!).                                                                                                         PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_COUNT      = 800      # Number of LED pixels.
+LED_PIN        = 10  # GPIO pin connected to the pixels (18 uses PWM!).                                                                                                         PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 30     # Set to 0 for darkest and 255 for brightest
@@ -247,7 +247,6 @@ if __name__ == '__main__':
                     current_letter = [x + 200 for x in current_letter]
                     display_letter(current_letter, Color(150, 150,150))
                 elif current_tile == 4:
-                    print("heere")
                     current_letter = light.letter_arrays[letter]
                     current_letter = [x + 300 for x in current_letter]
                     display_letter(current_letter, Color(150, 150,150))
