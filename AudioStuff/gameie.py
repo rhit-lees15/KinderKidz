da8ce1037c512df2a96c768aea6aa3816cde3274
KinderKidz <abufarna@rose-hulman.edu>
2024-05-14 01:05:02 -0400

    trial code

--- AudioStuff/gameie.py
+++ AudioStuff/gameie.py
@@ -1,6 +1,6 @@
 
 import argparse
-# import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO 
 import vlc
 import time
 import string
@@ -9,19 +9,20 @@ from pygame import Color
 from rpi_ws281x import *
 import game_sound as gamesound
 
-# Initialize lights
-# LED strip configuration:
-LED_COUNT      = 300      # Number of LED pixels.
-LED_PIN        = 10  # GPIO pin connected to the pixels (18 uses PWM!).                                                                                                         PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 65     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+# # Initialize lights
+# # LED strip configuration:
+# LED_COUNT      = 300      # Number of LED pixels.
+# LED_PIN        = 10  # GPIO pin connected to the pixels (18 uses PWM!).                                                                                                         PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+# LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+# LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+# LED_BRIGHTNESS = 65     # Set to 0 for darkest and 255 for brightest
+# LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+# LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
     
 
 # GPIO Pins for buttons
+# BUTTON_PINS = [17, 27, 22, 23, 24, 25, 16, 26]
 BUTTON_PINS = [17, 27, 22, 23, 24, 25, 16, 26]
 
 # Function to generate a random word
@@ -202,18 +203,18 @@ for idx, pin in enumerate(BUTTON_PINS):
 button_sequence = [BUTTON_PINS[randomizedLetters.index(letter)] for letter in randomWord]
 
 if __name__ == '__main__':
-    #####* Start the game
+    # #####* Start the game
 
-    # Initialization of lights    
-    # Process arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
-    args = parser.parse_args()
+    # # Initialization of lights    
+    # # Process arguments
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    # args = parser.parse_args()
 
-    # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-    # Intialize the library (must be called once before other functions).
-    strip.begin()
+    # # Create NeoPixel object with appropriate configuration.
+    # strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # # Intialize the library (must be called once before other functions).
+    # strip.begin()
 
 
     print("Welcome to the Word Spelling Game!")
