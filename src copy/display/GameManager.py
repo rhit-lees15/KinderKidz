9efdeca8eb318ba29abbd96f65a291e59ff48bf5
Noor Abufares <abufarna@rose-hulman.edu>
2024-10-09 14:48:01 -0400

    defined terms

--- src copy/display/GameManager.py
+++ src copy/display/GameManager.py
@@ -12,15 +12,14 @@ from display.MyGUI import GUI
 # from Media.GameSound import Audio
 
 class Game:
-    
+    # Dictionary of all the words included in the spelling game
+    word_list = ['MY', 'THIS', 'A', 'IS', 'HOME','THE', 'IN', 'CITY', 'BY', 'OCEAN','ON', 'NOT', 'FARM', 'LIKE', 'I']
+    BUTTON_PINS = [24, 25, 23, 22, 5, 6, 13, 12]  
+      
     GPIO.setmode(GPIO.BCM)
     for pin in BUTTON_PINS:
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(pin, GPIO.FALLING, callback=lambda pin: buttonPress(pin), bouncetime=1000)
-
-    # Dictionary of all the words included in the spelling game
-    word_list = ['MY', 'THIS', 'A', 'IS', 'HOME','THE', 'IN', 'CITY', 'BY', 'OCEAN','ON', 'NOT', 'FARM', 'LIKE', 'I']
-    BUTTON_PINS = [24, 25, 23, 22, 5, 6, 13, 12]
     
     words_remaining = True
     random.shuffle(word_list)
