c01bc88d7e2c05210f51eead3599e2ae1bc7aca7
Solbin Lee <lees15@rose-hulman.edu>
2024-10-20 15:54:26 -0400

    New

---
+++ src final/main.py
@@ -0,0 +1,15 @@
+import game
+
+def main():
+    print("Starting the Game")
+    game.init()
+    # begin start screen
+        # initialize sound
+        # initialize lights
+
+
+
+
+
+if __name__ == "__main__":
+    main()
