b1e8778900797bdc28338470e45926e58a2ec8c6
Noor Abufares <abufarna@rose-hulman.edu>
2024-10-24 14:25:16 -0400

    old screen

--- src_final/screens.py
+++ src_final/screens.py
@@ -2,12 +2,6 @@ import sys
 import pygame
 import random
 
-# Full screen the GUI to fill the screen
-display = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-
-# Screen dimensions
-screen_width, screen_height = display.get_size()
-
 class MainScreen:
     # def __init__(self, game):
     #     self.game = game
@@ -20,6 +14,12 @@ class MainScreen:
  ################### THIS IS A TEST TO SEE IF NEW CHANGES ARE SAVED #####################
     def __init__(self, game):
         self.game = game
+       
+        # Full screen the GUI to fill the screen
+        self.display = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+ 
+        # Screen dimensions
+        self.screen_width, self.screen_height = self.display.get_size()
  
         # Button setup
         self.font = pygame.font.Font(None, 50)
@@ -77,25 +77,13 @@ class TimerScreen:
         self.game = game
         self.font = pygame.font.Font(None, 50)
 
-        # # Button setup for 1 minute, 3 minutes, 5 minutes
-        # self.one_min_button = pygame.Rect(game.screen_width // 2 - 100, game.screen_height // 2 - 100, 200, 60)
-        # self.three_min_button = pygame.Rect(game.screen_width // 2 - 100, game.screen_height // 2, 200, 60)
-        # self.five_min_button = pygame.Rect(game.screen_width // 2 - 100, game.screen_height // 2 + 100, 200, 60)
+        # Button setup for 1 minute, 3 minutes, 5 minutes
+        self.one_min_button = pygame.Rect(game.screen_width // 2 - 100, game.screen_height // 2 - 100, 200, 60)
+        self.three_min_button = pygame.Rect(game.screen_width // 2 - 100, game.screen_height // 2, 200, 60)
+        self.five_min_button = pygame.Rect(game.screen_width // 2 - 100, game.screen_height // 2 + 100, 200, 60)
 
-        # self.add_word_button = pygame.Rect(game.screen_width // 2 - 100, game.screen_height // 2 + 180, 200, 60)
+        self.add_word_button = pygame.Rect(game.screen_width // 2 - 100, game.screen_height // 2 + 180, 200, 60)
 
-        # **** Modification
-        self.one_min_button = pygame.Rect(0, 0, 200, 60)
-        self.one_min_button.center = (screen_width // 3, screen_height // 2)
-        
-        self.three_min_button = pygame.Rect(0, 0, 200, 60)
-        self.three_min_button.center = (screen_width // 2, screen_height // 2)
-
-        self.five_min_button = pygame.Rect(0, 0, 200, 60)
-        self.five_min_button.center = (screen_width * (2 // 3), screen_height // 2)
-        
-        self.add_word_button = pygame.Rect(0, 0, 200, 60)
-        
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = event.pos
@@ -103,7 +91,6 @@ class TimerScreen:
                 self.game.switch_screen(AddWordScreen) 
             elif self.one_min_button.collidepoint(mouse_pos):
                 print("1-minute timer selected!")
-                # CHANGE THE TIMER HERE
                 self.game.switch_screen(lambda game: GameScreen(game, 10))
             elif self.three_min_button.collidepoint(mouse_pos):
                 print("3-minute timer selected!")
@@ -158,16 +145,14 @@ class GameScreen:
         # Timer setup
         self.start_time = pygame.time.get_ticks()
 
-        # # Quit button setup
-        # self.quit_button = pygame.Rect(game.screen_width // 2 - 100, game.screen_height - 120, 200, 80)
-        self.quit_button = pygame.Rect(0, 0, 200, 80)
-        self.quit_button.center = (screen_width // 2, screen_height // 2 + 75)
+        # Quit button setup
+        self.quit_button = pygame.Rect(game.screen_width // 2 - 100, game.screen_height - 120, 200, 80)
 
        # Initialize GPIO for buttons
         GPIO.setmode(GPIO.BCM)
         for pin in BUTTON_PINS.keys():
             GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.add_event_detect(pin, GPIO.FALLING, callback=self.gpio_button_pressed(pin), bouncetime=300)
+            GPIO.add_event_detect(pin, GPIO.FALLING, callback=self.gpio_button_pressed, bouncetime=300)
    
     def gpio_button_pressed(self, pin):
         """Handles the event when a GPIO button is pressed."""
@@ -255,19 +240,17 @@ class MusicScreen:
 
         # Buttons setup for songs
         self.song_buttons = [
-            # pygame.Rect(game.screen_width // 2 - 100, 50 + i * 60, 200, 50) for i in range(6)
-            pygame.Rect(game.screen_width // 2 - 100, 50 + i * 60, 200, 50) for i in range(5)
+            pygame.Rect(game.screen_width // 2 - 100, 50 + i * 60, 200, 50) for i in range(6)
         ]
         self.songs = [
             "Audio/Songs/twinkle-twinkle.mp3",
             "Audio/Songs/happy-and-you-know-it.mp3",
-            # "Audio/Songs/idk.mp3",
+            "Audio/Songs/idk.mp3",
             "Audio/Songs/my-year-zombies.mp3",
             "Audio/Songs/puff-the-magic-dragon.mp3",
             "Audio/Songs/body-bop-bop.mp3"
         ]
-        # self.song_labels = [f"Song {i+1}" for i in range(6)]
-        self.song_labels = [f"Song {i+1}" for i in range(5)]
+        self.song_labels = [f"Song {i+1}" for i in range(6)]
 
         # "Choose Your Own!" button
         self.choose_button = pygame.Rect(game.screen_width - 250, game.screen_height - 80, 200, 50)
